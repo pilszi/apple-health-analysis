a3cc63e178c5c224901ca8ac06050206bcf21329
pilszi <jungwp1006@gmail.com>
2026-07-08 14:19:58 +0900

    전처리 과정 수정 완료

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -9,81 +9,116 @@ def PreProcess():
         (결측치, 중복값, 불필요한 데이터 삭제)
 
     """
-    file_path = f'./static/data/apple_health_export'
-    model = LinearRegression()
-    result = 0
-    
-    # 1. 이상치 처리
     try:
+        file_path = f'./static/data/apple_health_export'
+        model = LinearRegression()
+        result = 0
         df_workout = pd.read_csv(f"{file_path}/workout.csv")
         df_hr = pd.read_csv(f"{file_path}/hr.csv")
+        
+        # 1. 이상치 처리
+        try:
+            # workout 이상치 처리
+            
+            # 1. 운동시간 20분 미만인 데이터 제거
+            min_value = df_workout["duration"] >= 20
+            df_workout = df_workout[min_value]
+            
+            # 2. 갯수가 부족한 운동타입 제거
+            value = df_workout["workoutActivityType"].value_counts()
+            valid_types = value[value >= 20].index
+            df_workout = df_workout[df_workout["workoutActivityType"].isin(valid_types)]
 
-        # 기초 대사 칼로리 이상치 제거
-        df_workout = df_workout[df_workout["BasalEnergyBurned"] < 2000]
+            # 3. 기초 대사 칼로리 이상치 제거
+            df_workout = df_workout[df_workout["BasalEnergyBurned"] < 2000]
 
-        model.fit(df_workout['duration'].values.reshape(-1, 1), df_workout['ActiveEnergyBurned'])
-        result = model.predict(df_workout['duration'].values.reshape(-1,1))
-        # 실제 값과 예측값의 오차의 절대값을 저장
-        df_workout['y_pred'] = result
-        df_workout['error'] = np.abs(df_workout['ActiveEnergyBurned'] - df_workout['y_pred'])
-        top_errors = df_workout.sort_values(by='error', ascending=False).head(4)
-        print(top_errors[['workoutActivityType', 'date', 'duration', 'ActiveEnergyBurned', 'error']])
+            # 4. 활동 칼로리 이상치 확인
+            model.fit(df_workout['duration'].values.reshape(-1, 1), df_workout['ActiveEnergyBurned'])
+            result = model.predict(df_workout['duration'].values.reshape(-1,1))
+            # 실제 값과 예측값의 오차의 절대값을 저장
+            df_workout['y_pred'] = result
+            df_workout['error'] = np.abs(df_workout['ActiveEnergyBurned'] - df_workout['y_pred'])
+            top_errors = df_workout.sort_values(by='error', ascending=False).head(4)
+            print(top_errors[['workoutActivityType', 'date', 'duration', 'ActiveEnergyBurned', 'error']])
 
-        # 그래프로 시각화하여 확인하기
-        plt.figure(figsize=(10, 6))
-        plt.scatter(df_workout['duration'], df_workout['ActiveEnergyBurned'], c= 'b')
-        plt.plot(df_workout['duration'], result, c='r')
-        # 가장 멀리 떨어진 점들만 빨간색 테두리로 강조 표시
-        plt.scatter(top_errors['duration'], top_errors['ActiveEnergyBurned'], 
-                    color='none', edgecolor='red', s=150)
-        # 강조된 점들에 오차 적어주기
-        for idx, row in top_errors.iterrows():
-            plt.vlines(x=row['duration'], ymin=row['ActiveEnergyBurned'], ymax=row['y_pred'], colors='gray', linestyles='dashed', alpha=0.4)
-            plt.text(row['duration']- 5, row['ActiveEnergyBurned']- 80, f"error: {row['error']:.1f}")
-        plt.xlabel("duration")
-        plt.ylabel("ActiveEnergyBurned")
-        plt.savefig(f"./static/img/workout.png", dpi=300, bbox_inches="tight")
+            # 4-1. 그래프로 시각화하여 확인하기
+            plt.figure(figsize=(10, 6))
+            plt.scatter(df_workout['duration'], df_workout['ActiveEnergyBurned'], c= 'b')
+            plt.plot(df_workout['duration'], result, c='r')
+            # 가장 멀리 떨어진 점들만 빨간색 테두리로 강조 표시
+            plt.scatter(top_errors['duration'], top_errors['ActiveEnergyBurned'], 
+                        color='none', edgecolor='red', s=150)
+            # 4-2. 강조된 점들에 오차 적어주기
+            for idx, row in top_errors.iterrows():
+                plt.vlines(x=row['duration'], ymin=row['ActiveEnergyBurned'], ymax=row['y_pred'], colors='gray', linestyles='dashed', alpha=0.4)
+                plt.text(row['duration']- 5, row['ActiveEnergyBurned']- 80, f"error: {row['error']:.1f}")
+            plt.xlabel("duration")
+            plt.ylabel("ActiveEnergyBurned")
+            plt.savefig(f"./static/img/workout.png", dpi=300, bbox_inches="tight")
 
-        # 활동 칼로리 이상치 제거
-        df_workout = df_workout.drop([2400, 1958])
+            # 4-3. 활동 칼로리 이상치 제거
+            df_workout = df_workout.drop([2400, 1958])
+        except KeyError as k:
+            print(f"key 가 틀렸습니다. : {k}")
+        except SyntaxError as s:
+            print(f"문법 오류 : {s}")
+        except Exception as e:
+            print(f"기타 에러 발생 : {e}")
+        
+        # 2. 결측치 처리
+        try:    
+            # workout 결측치 처리 - 온도, 습도 결측치는 선형보간으로 채우고, 그 외 컬럼 결측치는 행 제거
+            nan_sum = df_workout.isna().sum()
+            print(nan_sum)
+            weather_cols = ['Temperature', 'Humidity']
+            other_cols = [col for col in df_workout.columns if col not in weather_cols]
+            df_workout = df_workout.sort_values("startDate")
+            df_workout[weather_cols] = df_workout[weather_cols].interpolate(method="linear", limit_direction="both")
+            df_workout.dropna(subset=other_cols, inplace=True)
+
+            # workout 중복값 처리 - 처음 값만 남기고 모두 제거
+            print(df_workout.duplicated().sum())
+            if df_workout.duplicated().sum() > 0:
+                df_workout.drop_duplicates(inplace=True, keep='first')
+            print("="*60)
+            print(f"==== workout_pre.csv 생성/ 행 : {len(df_workout)} ====")
+            
+        except KeyError as k:
+            print(f"key 가 틀렸습니다. : {k}")
+        except SyntaxError as s:
+            print(f"문법 오류 : {s}")
+        except Exception as e:
+            print(f"기타 에러 발생 : {e}")
+
+        # 심박수 데이터 전처리 - 전체 심박수 중 운동 중 심박수만 남기고 모두 제거
+        try:
+            df_hr['startDate'] = pd.to_datetime(df_hr['startDate'], format='mixed', errors='coerce')
+            df_workout['startDate'] = pd.to_datetime(df_workout['startDate'], format='mixed', errors='coerce')
+            df_workout['endDate'] = pd.to_datetime(df_workout['endDate'], format='mixed', errors='coerce')
+
+            hr_li = []
+            for idx, workout in df_workout.iterrows():
+                start = workout['startDate']
+                end = workout['endDate']
+                match_hr = df_hr[df_hr['startDate'].between(start, end)]
+                hr_li.append(match_hr)
+            df_hr_workout = pd.concat(hr_li).drop_duplicates().sort_values('startDate')
+            print(f"==== hr_pre.csv 생성/ 행 : {len(df_hr_workout)} ====")
+        except KeyError as k:
+            print(f"key 가 틀렸습니다. : {k}")
+        except SyntaxError as s:
+            print(f"문법 오류 : {s}")
+        except Exception as e:
+            print(f"기타 에러 발생 : {e}")
+        result = 1
+
     except FileNotFoundError as f:
         print(f"파일을 찾지 못했습니다 : {f}")
     except KeyError as k:
-        print(f"key 가 틀렸습니다. : {k}")
-    except Exception as e:
-        print(f"기타 에러 발생 : {e}")
-    
-    # 2. 결측치 처리
-    try:    
-        df_li = [(df_workout, "workout"), (df_hr, "hr")]
-        for df, filename in df_li:
-            print(f'{df.head()}')
-
-            # 결측치 확인
-            print(df.info())
-            if df.isna().sum().sum() > 0:
-                df.dropna(inplace=True)
-            print("="*60)
-
-            # 중복값 확인
-            print(df.duplicated().sum())
-            if df.duplicated().sum() > 0:
-                df.drop_duplicates(inplace=True)
-            print("="*60)
-
-            # 모든 데이터 날짜 맞추기
-            print(f"2021-5-15 이전 데이터 수 = {len(df[df['date'] < '2021-05-15'])}")
-            df = df[df["date"] >= "2021-05-15"]
-            print(df.head())
-            df.to_csv(f'./static/data/apple_health_export/{filename}_pre.csv', index=False, encoding="utf-8-sig")
-            print(f"==== {filename}_pre.csv 생성 ====")
-        result = 1
-    except KeyError as k:
-        print(f"key 가 틀렸습니다. : {k}")
+            print(f"key 가 틀렸습니다. : {k}")
     except SyntaxError as s:
         print(f"문법 오류 : {s}")
     except Exception as e:
-        print(f"기타 에러 발생 : {e}")
+            print(f"기타 에러 발생 : {e}")
+        
     return result
-
-PreProcess()
--- src/xml_convert.py
+++ src/xml_convert.py
@@ -7,138 +7,134 @@ def convert_xml():
     """
         xml 파일에서 특정 attrib 만 추출하여 csv 파일로 저장하는 함수
     """
-    file_path = "./static/data/apple_health_export/export.xml"
-    workouts = []
-    heart_rates = []
-    result = 0  
     try:
+        file_path = "./static/data/apple_health_export/export.xml"
+        workouts = []
+        heart_rates = []
+        result = 0  
         context = ET.iterparse(file_path, events=("end",))
-        print(f"⏳ [Apple Watch] 데이터만 필터링하여 스캔 시작...")
-        for event, elem in context:
+        try:
+            print(f"⏳ [Apple Watch] 데이터만 필터링하여 스캔 시작...")
+            for event, elem in context:
 
-            # 1. 운동 세션 추출 (지정한 애플워치 기록만)
-            if elem.tag == "Workout":
-                if "Apple Watch" in elem.attrib.get("sourceName"):
-                # if elem.attrib.get("sourceName") == target_source:
-                    workout_data = dict(elem.attrib)
-                    
-                    # 자식 요소(MetadataEntry)들을 순회하면서 HKAverageMETs 검색
-                    for child in elem:
-                        if child.tag == "WorkoutStatistics":
-                            # 'key'가 아니라 'type' 속성을 확인해야 합니다.
-                            stat_type = child.attrib.get("type")
-                            
-                            if stat_type == "HKQuantityTypeIdentifierActiveEnergyBurned":
-                                # 'value'가 아니라 'sum' 속성에서 가져옵니다.
-                                sum_value = child.attrib.get("sum")
-                                if sum_value:
-                                    try:
-                                        # 이미 숫자 형식 문자열이므로 바로 float 변환이 가능합니다.
-                                        workout_data["ActiveEnergyBurned"] = float(sum_value)
-                                    except ValueError:
-                                        workout_data["ActiveEnergyBurned"] = sum_value
-                                        
-                            elif stat_type == "HKQuantityTypeIdentifierBasalEnergyBurned":
-                                sum_value = child.attrib.get("sum")
-                                if sum_value:
-                                    try:
-                                        workout_data["BasalEnergyBurned"] = float(sum_value)
-                                    except ValueError:
-                                        workout_data["BasalEnergyBurned"] = sum_value
-                        elif child.tag == "MetadataEntry":
-                            meta_key = child.attrib.get("key")
-                            if meta_key == "HKWeatherTemperature":
-                                key_value = child.attrib.get("value")
-                                if key_value:
-                                    value = "".join(v for v in key_value if v.isdigit() or v in ['.', '-'])
-                                    # 화씨로 저장된 온도 섭씨로 변환
-                                    workout_data["Temperature"] = (float(value) - 32) / 1.8
-                                else:
-                                    workout_data["Temperature"] = None
+                # 1. 운동 세션 추출 (지정한 애플워치 기록만)
+                if elem.tag == "Workout":
+                    if "Apple Watch" in elem.attrib.get("sourceName"):
+                    # if elem.attrib.get("sourceName") == target_source:
+                        workout_data = dict(elem.attrib)
+                        
+                        # 자식 요소(MetadataEntry)들을 순회하면서 HKAverageMETs 검색
+                        for child in elem:
+                            if child.tag == "WorkoutStatistics":
+                                # 'key'가 아니라 'type' 속성을 확인해야 합니다.
+                                stat_type = child.attrib.get("type")
+                                
+                                if stat_type == "HKQuantityTypeIdentifierActiveEnergyBurned":
+                                    # 'value'가 아니라 'sum' 속성에서 가져옵니다.
+                                    sum_value = child.attrib.get("sum")
+                                    if sum_value:
+                                        try:
+                                            # 이미 숫자 형식 문자열이므로 바로 float 변환이 가능합니다.
+                                            workout_data["ActiveEnergyBurned"] = float(sum_value)
+                                        except ValueError:
+                                            workout_data["ActiveEnergyBurned"] = sum_value
+                                            
+                                elif stat_type == "HKQuantityTypeIdentifierBasalEnergyBurned":
+                                    sum_value = child.attrib.get("sum")
+                                    if sum_value:
+                                        try:
+                                            workout_data["BasalEnergyBurned"] = float(sum_value)
+                                        except ValueError:
+                                            workout_data["BasalEnergyBurned"] = sum_value
+                            elif child.tag == "MetadataEntry":
+                                meta_key = child.attrib.get("key")
+                                if meta_key == "HKWeatherTemperature":
+                                    key_value = child.attrib.get("value")
+                                    if key_value:
+                                        value = "".join(v for v in key_value if v.isdigit() or v in ['.', '-'])
+                                        # 화씨로 저장된 온도 섭씨로 변환
+                                        workout_data["Temperature"] = (float(value) - 32) / 1.8
+                                    else:
+                                        workout_data["Temperature"] = None
 
-                            elif meta_key ==  "HKWeatherHumidity":
-                                key_value = child.attrib.get("value")
-                                if key_value:
-                                    value = "".join(v for v in key_value if v.isdigit() or v in ['.', '-'])
-                                    # 저장된 습도 값은 5600 %  같이 100이 곱해진 값
-                                    workout_data["Humidity"] = float(value) / 100
-                                else:
-                                    workout_data["Humidity"] = None
-                    # 가공된 딕셔너리를 기존 바구니에 담기
+                                elif meta_key ==  "HKWeatherHumidity":
+                                    key_value = child.attrib.get("value")
+                                    if key_value:
+                                        value = "".join(v for v in key_value if v.isdigit() or v in ['.', '-'])
+                                        # 저장된 습도 값은 5600 %  같이 100이 곱해진 값
+                                        workout_data["Humidity"] = float(value) / 100
+                                    else:
+                                        workout_data["Humidity"] = None
+                        # 가공된 딕셔너리를 기존 바구니에 담기
 
-                    workouts.append(workout_data)
-                elem.clear()
-                # print("=== Workout 발견 ===")
+                        workouts.append(workout_data)
+                    elem.clear()
+                    # print("=== Workout 발견 ===")
 
-            # 2. 상세 기록 추출 (지정한 애플워치 기록 + 심박수만)
-            elif elem.tag == "Record":
-                if ("Apple Watch" in elem.attrib.get("sourceName") and 
-                    elem.attrib.get("type") == "HKQuantityTypeIdentifierHeartRate"):
-                    # 후속 조인(Join)과 시각화를 위해 타임스탬프와 심박수 값만 정제해서 보관
-                    heart_rates.append({
-                        'heart_rate': float(elem.attrib.get('value')),
-                        'startDate': elem.attrib.get('startDate'),
-                        'sourceName': elem.attrib.get('sourceName') # 확인용
-                    })
-        
-                # 메모리 해제
-                    # print("=== HeartRate 발견 ===")
-                elem.clear()
-    except FileNotFoundError as f:
-        print(f"파일을 찾지 못했습니다 : {f}")
-    except KeyError as k:
-        print(f"key 가 틀렸습니다. : {k}")
-    except Exception as e:
-        print(f"기타 에러 발생 : {e}")
+                # 2. 상세 기록 추출 (지정한 애플워치 기록 + 심박수만)
+                elif elem.tag == "Record":
+                    if ("Apple Watch" in elem.attrib.get("sourceName") and 
+                        elem.attrib.get("type") == "HKQuantityTypeIdentifierHeartRate"):
+                        # 후속 조인(Join)과 시각화를 위해 타임스탬프와 심박수 값만 정제해서 보관
+                        heart_rates.append({
+                            'heart_rate': float(elem.attrib.get('value')),
+                            'startDate': elem.attrib.get('startDate'),
+                            'sourceName': elem.attrib.get('sourceName') # 확인용
+                        })
+            
+                    # 메모리 해제
+                        # print("=== HeartRate 발견 ===")
+                    elem.clear()
+        except KeyError as k:
+            print(f"key 가 틀렸습니다. : {k}")
+        except SyntaxError as s:
+            print(f"문법 오류 : {s}")
+        except Exception as e:
+            print(f"기타 에러 발생 : {e}")
 
-    try:    
-        # 데이터프레임 변환
-        df_workout = pd.DataFrame(workouts)
-        df_hr = pd.DataFrame(heart_rates)
+        # 추출한 데이터 정리 후 csv 파일로 저장
+        try:    
+            # 데이터프레임 변환
+            df_workout = pd.DataFrame(workouts)
+            df_hr = pd.DataFrame(heart_rates)
 
-        workout_cols = ["sourceName", "sourceVersion", "device", "durationUnit", "creationDate"]
-        hr_cols = ["sourceName"]
+            workout_cols = ["sourceName", "sourceVersion", "device", "durationUnit", "creationDate"]
+            hr_cols = ["sourceName"]
 
-        # 3. 운동 세션 정제 (타임존 정보가 포함된 시간을 판다스 시간 객체로 변환)
-        if not df_workout.empty:
-            df_workout["startDate"] = pd.to_datetime(df_workout["startDate"], errors='coerce')
-            df_workout["endDate"] = pd.to_datetime(df_workout["endDate"], errors='coerce')
-            df_workout["date"] = df_workout["startDate"].dt.date  # 조인용 Key 컬럼 생성
-            # 운동 타입 불필요 단어 제거
-            df_workout["workoutActivityType"] = df_workout[
-                "workoutActivityType"
-            ].str.replace("HKWorkoutActivityType", "", regex=False)
-            # 온도, 습도 컬럼 None 값 근처 날짜(전날과 다음날) 값을 활용해 채우기
-            df_workout = df_workout.sort_values("startDate")
-            df_workout["Temperature"] = df_workout["Temperature"].interpolate(method="linear", limit_direction='both').round(1)
-            df_workout["Humidity"] = df_workout["Humidity"].interpolate(method="linear", limit_direction='both').round(1)
-            # workoutActivityType 컬럼 값 불필요한 반복 단어 제거
-            df_workout["workoutActivityType"] = df_workout["workoutActivityType"].str.replace("HKWorkoutActivityType", "", regex=False)
-            # 데이터프레임 별 불필요한 컬럼 제거
-            df_workout = df_workout.drop(columns=workout_cols, errors='ignore')
-            print("=== Workout ===")
-            df_workout.to_csv("./static/data/apple_health_export/workout.csv", index=False, encoding="utf-8-sig")
+            # 3. 운동 세션 컬럼 정리
+            if not df_workout.empty:
+                df_workout["startDate"] = pd.to_datetime(df_workout["startDate"], format='mixed', errors='coerce').dt.tz_localize(None)
+                df_workout["endDate"] = pd.to_datetime(df_workout["endDate"], format='mixed', errors='coerce').dt.tz_localize(None)
+                df_workout["date"] = df_workout["startDate"].dt.date
+                # 운동 타입 불필요 단어 제거
+                df_workout["workoutActivityType"] = df_workout[
+                    "workoutActivityType"
+                ].str.replace("HKWorkoutActivityType", "", regex=False)
+                
+                # workoutActivityType 컬럼 값 불필요한 반복 단어 제거
+                df_workout["workoutActivityType"] = df_workout["workoutActivityType"].str.replace("HKWorkoutActivityType", "", regex=False)
+                # 데이터프레임 별 불필요한 컬럼 제거
+                df_workout = df_workout.drop(columns=workout_cols, errors='ignore')
+                print("=== Workout ===")
+                df_workout.to_csv("./static/data/apple_health_export/workout.csv", index=False, encoding="utf-8-sig")
 
-        # 4. 상세 심박수 정제 
-        if not df_hr.empty:
-            df_hr['startDate'] = pd.to_datetime(df_hr['startDate'], errors='coerce')
-            df_hr['date'] = df_hr['startDate'].dt.date
-            df_hr = df_hr.drop(columns=hr_cols, errors='ignore')
-            print("=== HeartRate ===")
-            df_hr.to_csv("./static/data/apple_health_export/hr.csv", index=False, encoding="utf-8-sig")
-        
+            # 4. 심박수 데이터 컬럼 정리
+            if not df_hr.empty:
+                df_hr['startDate'] = pd.to_datetime(df_hr['startDate'], format='mixed', errors='coerce').dt.tz_localize(None)
+                df_hr['date'] = df_hr['startDate'].dt.date
+                df_hr = df_hr.drop(columns=hr_cols, errors='ignore')
+                print("=== HeartRate ===")
+                df_hr.to_csv("./static/data/apple_health_export/hr.csv", index=False, encoding="utf-8-sig")
+            print(f"Workout 행수: {len(df_workout)} | HR 행수: {len(df_hr)}")
+        except KeyError as k:
+            print(f"key 가 틀렸습니다. : {k}")
+        except SyntaxError as s:
+            print(f"문법 오류 : {s}")
+        except Exception as e:
+            print(f"기타 에러 발생 : {e}")
         result = 1
 
-        print("✅ 모든 날짜/시간 컬럼이 성공적으로 정제되었습니다.")
-        print(f"Workout 행수: {len(df_workout)} | HR 행수: {len(df_hr)}")
-    
-    except KeyError as k:
-        print(f"key 가 틀렸습니다. : {k}")
-    except Exception as e:
-        print(f"기타 에러 발생 : {e}")
+    except FileNotFoundError as f:
+        print(f'파일을 찾지 못했습니다. : {f}')
 
     return {"result": result}
-
-
-
-
